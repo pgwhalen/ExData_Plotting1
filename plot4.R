00a5476194fa9ccff481572b41e79611c8eb7dce
pgwhalen <pgwhalen@gmail.com>
2015-04-12 18:47:08 -0500

    finished with plot4

--- plot4.R
+++ plot4.R
@@ -7,6 +7,17 @@ power$Datetime <- strptime(power$Datetime, "%d/%m/%Y|%H:%M:%S")
 # set is just the two days we care about
 set <- subset(power, power$Datetime$year == 107 & power$Datetime$mon == 1 & (power$Datetime$mday == 1 | power$Datetime$mday == 2))
 
-png('plot3.png')
+png('plot4.png')
+par(mfrow = c(2,2))
+plot(set$Datetime, set$Global_active_power, type ="l", xlab="", ylab="Global Active Power (kilowatts)")
+plot(set$Datetime, set$Voltage, type ="l", xlab="", ylab="Voltage")
+
+plot(set$Datetime, set$Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub meterting")
+lines(set$Datetime, set$Sub_metering_1, col = "black")
+lines(set$Datetime, set$Sub_metering_2, col = "red")
+lines(set$Datetime, set$Sub_metering_3, col = "blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c('black', 'red', 'blue'), lty = c(1,1), bty = "n")
+
+plot(set$Datetime, set$Global_reactive_power, type ="l", xlab="", ylab="Global_reactive_power")
 
 dev.off()
